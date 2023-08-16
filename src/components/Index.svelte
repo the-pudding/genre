1fea1ea321f1e1ad7d32f49235dd79e1a151865e
Matt <matt@matt.mynetworksettings.com>
2023-08-16 17:04:40 -0400

    update github pages

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,83 @@
 <script>
-	import { getContext } from "svelte";
+	import { getContext, onMount } from "svelte";
 	import Demo from "$components/demo/Demo.svelte";
 	import WIP from "$components/helpers/WIP.svelte";
 	// import Footer from "$components/Footer.svelte";
-
+	import GenreData from '$data/rank.csv'
 	// const copy = getContext("copy");
 	// const data = getContext("data");
+	let dataForChart;
+	onMount(() => {
+
+		let rankings = [];
+		GenreData.forEach(d => {
+			let date = d["genre"];
+			let genres = Object.keys(d);
+			genres = genres.filter(d => {
+				return d != "genre";
+			})
+			let toPush = {date:date,ranking:[]};
+			genres.filter(k => {
+				return +d[k] > 0;
+			}).forEach(j => {
+				toPush.ranking.push({genre:j,rank:+d[j]});
+			})
+
+			toPush["ranking"] = toPush["ranking"].sort((a,b) => {
+				return a.rank - b.rank;
+			})
+
+			rankings.push(toPush);
+		})
+		dataForChart = rankings;
+	})
 </script>
 
-<WIP />
-<Demo />
-<!-- <Footer /> -->
+{#if dataForChart}
+<table>
+	<tbody>
+	<tr class="wrapper">
+		{#each dataForChart as item}
+			<td class="col">
+				<p>{item.date}</p>
+				<!-- <div class="genre-ranking"> -->
+					{#each item.ranking.slice(0,500) as genre}
+						<div class="item">{genre.genre} ({genre.rank})</div>
+					{/each}
+				<!-- </div> -->
+			</td>
+			
+		{/each}
+	</tr>
+	</tbody>
+</table>
+{/if}
+
+<style>
+	table {
+		width: auto;
+    	overflow: scroll;
+		margin-left: 10px;
+	}
+.wrapper {
+	/* display: flex; */
+}
+.col {
+	/* display: flex;
+	flex-direction: column;
+	margin-right: 10px; */
+}
+
+.genre-ranking {
+	/* display: flex;
+	flex-direction: column; */
+}
+
+.item {
+	white-space: nowrap;
+	/* display: inline-block; */
+}
+
+</style>
+<!-- <WIP />
+<Demo /> -->
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,30 @@
+// import data from '$data/rank.csv'
+
 export async function load() {
+
+	// let rankings = [];
+	// data.forEach(d => {
+	// 	console.log(d)
+	// 	let date = d["genre"];
+	// 	let genres = Object.keys(d);
+	// 	genres = genres.filter(d => {
+	// 		return d != "genre";
+	// 	})
+	// 	let toPush = {date:date,ranking:[]};
+	// 	genres.filter(k => {
+	// 		return +d[k] > 0;
+	// 	}).forEach(j => {
+	// 		toPush.ranking.push({genre:j,rank:+d[j]});
+	// 	})
+
+	// 	toPush["ranking"] = toPush["ranking"].sort((a,b) => {
+	// 		return a.rank - b.rank;
+	// 	})
+
+	// 	rankings.push(toPush);
+	// })
+
+	// return { data };
 	const data = ["a", "b", "c"];
 	return { data };
 }
