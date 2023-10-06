a1946c3a1a6b07fef6f7605032b22dd6d15e6931
Michelle McGhee <michmac202@gmail.com>
2023-10-06 13:35:24 -0700

    updated chapter markers closes #11

--- src/components/Chapters.svelte
+++ src/components/Chapters.svelte
@@ -2,19 +2,23 @@
 	import copy from "$data/copy.json";
 	import { activeSlide } from "$stores/misc.js";
 	import _ from "lodash";
+
+	const chapters = _.groupBy(copy.slides, "section");
 </script>
 
 <div class="chapters">
-	{#each copy.slides as { section }, i}
-		{@const sectionStarter = i === 0 || section > copy.slides[i - 1].section}
-		{@const sectionActive = copy.slides[$activeSlide].section === section}
-		<div class="block" class:active={i === $activeSlide}>
-			{#if sectionStarter}
-				<div class="title" class:active={sectionActive}>
-					{section}<span class:visible={sectionActive}>
-						{" — "}{copy.sections[section]}</span
-					>
-				</div>
+	{#each Object.keys(chapters) as chapter}
+		{@const chapterActive = copy.slides[$activeSlide].section === chapter}
+		<div class="chapter" class:active={chapterActive}>
+			<span class="title">
+				{chapter} - {copy.sections[chapter]}
+			</span>
+			{#if chapterActive}
+				{#each chapters[chapter] as slide}
+					<div class="block" class:active={$activeSlide + 1 === +slide.slide} />
+				{/each}
+			{:else}
+				<div class="block" />
 			{/if}
 		</div>
 	{/each}
@@ -29,6 +33,37 @@
 		justify-content: space-between;
 		padding: 0 1rem;
 	}
+	.chapter {
+		display: flex;
+		align-items: end;
+		flex: 1;
+		margin: 0 3px;
+	}
+	.chapter:first-of-type {
+		margin-left: 0;
+	}
+	.chapter:last-of-type {
+		margin-right: 0;
+	}
+	.chapter.active {
+		flex: 5;
+	}
+	.chapter.active .title {
+		opacity: 1;
+	}
+	.title {
+		position: absolute;
+		top: -1.2rem;
+		font-size: 12px;
+		white-space: nowrap;
+		opacity: 0.3;
+	}
+	.title span {
+		visibility: hidden;
+	}
+	span.visible {
+		visibility: visible;
+	}
 	.block {
 		position: relative;
 		flex: 1;
@@ -37,32 +72,16 @@
 		background: rgba(0, 0, 0, 0.3);
 		transition: background calc(var(--1s) * 0.4);
 	}
-	.block:first-child {
+	.block:first-of-type {
 		margin-left: 0;
 	}
-	.block:last-child {
+	.block:last-of-type {
 		margin-right: 0;
 	}
 	.block.active {
 		background: rgba(0, 0, 0, 1);
 	}
-	.title {
-		position: absolute;
-		bottom: 0.5rem;
-		left: 0;
-		font-size: 12px;
-		white-space: nowrap;
-		opacity: 0.3;
-	}
-	.title.active {
-		opacity: 1;
-	}
-	.title span {
-		visibility: hidden;
-	}
-	span.visible {
-		visibility: visible;
-	}
+
 	@media (max-width: 600px) {
 		.title {
 			font-size: 10px;
