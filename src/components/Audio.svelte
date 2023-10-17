8aad0e4a5140f865ed115a8016129821ab8927da
Michelle McGhee <michmac202@gmail.com>
2023-10-16 18:30:57 -0700

    audio progress bar for non svgs

--- src/components/Audio.svelte
+++ src/components/Audio.svelte
@@ -16,7 +16,7 @@
 	$: onScreen = slide === $activeSlide;
 	$: url = copy.artists[id];
 	$: percentLeft =
-		duration && currentTime ? ((duration - currentTime) / duration) * 100 : 0;
+		duration && currentTime ? ((duration - currentTime) / duration) * 100 : 100;
 	$: padding = inline ? "4px 8px" : "10px 14px";
 	$: if (slide && !onScreen && audioEl) {
 		audioEl.pause();
@@ -36,13 +36,24 @@
 		} else audioEl.pause();
 		paused = !paused;
 	};
+	const onEnded = () => {
+		paused = true;
+		currentTime = 0;
+	};
 </script>
 
 <Button
+	color={"#EFEFEF"}
 	onClick={toggle}
-	style={`position: relative; display: inline; padding: ${padding};`}
+	style={`position: relative; display: inline; padding: ${padding}; color: transparent`}
 >
 	{label} 🎵
+	<div
+		class="progress"
+		class:curved={percentLeft === 100}
+		style:width={`${percentLeft}%`}
+	/>
+	<div class="words" style:padding>{label} 🎵</div>
 </Button>
 
 <audio
@@ -51,7 +62,34 @@
 	bind:this={audioEl}
 	bind:duration
 	bind:currentTime
+	on:ended={onEnded}
 />
 
 <style>
+	.progress {
+		background: var(--color-accent);
+		height: 100%;
+		top: 0;
+		left: 0;
+		position: absolute;
+		border-radius: 10px 0 0 10px;
+		transition: border-radius calc(var(--1s) * 2) ease-in-out;
+		z-index: 1;
+	}
+	.progress.curved {
+		transition: none;
+		border-radius: 10px;
+	}
+	.progress:hover {
+		background: #31ea86;
+	}
+	.words {
+		color: black;
+		z-index: 2;
+		top: 0;
+		left: 0;
+		position: absolute;
+		white-space: nowrap;
+		pointer-events: none;
+	}
 </style>
